a5dee3cc7c1e902609b6f53c04eda1dd4762c700
Hakan Serpen <hs4417@login-6.rcs.ic.ac.uk>
2021-02-23 01:45:23 +0000

    Runner Bash V2

--- Cylinder2DFlowControlWithRL/Run.sh
+++ Cylinder2DFlowControlWithRL/Run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#PBS -l walltime=24:00:00
+#PBS -l select=1:ncpus=1:mem=40gb
+cd $PBS_O_WORKDIR
 module load anaconda3
 module load anaconda3/personal
 source activate fenicsproject
-python3 single_runner.py
+python3 $PBS_O_WORKDIR/single_runner.py
